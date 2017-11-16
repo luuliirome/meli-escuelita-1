c97f9d3c75b91da2df0aa38269bd749a485d7305
curso <lurome_96@hotmail.com>
2017-11-16 15:00:57 -0300

    script

---
+++ scrpitSQL.sql
@@ -0,0 +1,4 @@
+insert usuario set id = 0 , version = 0 , mail = "tuvieja", password = "1234";
+
+
+insert publicacion set id = 3, version = 0 , autor_id = 1 , cantidad = 132, detalles = "hola mundo", imagen= "jaksd", nuevo = 0 , precio = 1234, titulo  = "holad";
