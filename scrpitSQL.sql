a13351627162226325005cf913e8350deb8cc034
curso <lurome_96@hotmail.com>
2017-11-17 12:12:15 -0300

    precioo

--- scrpitSQL.sql
+++ scrpitSQL.sql
@@ -1,6 +1,9 @@
-insert usuario set id = 0 , version = 0 , mail = "guidomaceira@gmail.com", password = "1234", reputacion=0;
-insert usuario set id = 0 , version = 0 , mail = "luliirome@gmail.com", password = "1234", reputacion=0;
-insert usuario set id = 0 , version = 0 , mail = "gregoemperador@gmail.com", password = "1234", reputacion=0;
-insert usuario set id = 0 , version = 0 , mail = "patoellongboarder@gmail.com", password = "1234", reputacion=0;
+insert usuario set id = 0 , version = 0 , mail = "guidomaceira@gmail.com", password = "1234", calificacion=0;
+insert usuario set id = 0 , version = 0 , mail = "luliirome@gmail.com", password = "1234", calificacion=0;
+insert usuario set id = 0 , version = 0 , mail = "gregoemperador@gmail.com", password = "1234", calificacion=0;
+insert usuario set id = 0 , version = 0 , mail = "patoellongboarder@gmail.com", password = "1234", calificacion=0;
+insert usuario set id = 0 , version = 0 , mail = "tuvieja", password = "1234", calificacion=0;
+insert usuario set id = 0 , version = 0 , mail = "luciainesromero@gmail.com", password = "987654321", calificacion=0;
+
 
 
